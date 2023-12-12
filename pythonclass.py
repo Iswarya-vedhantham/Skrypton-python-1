d10e0bb4d5fddfe0e61409275fffe24c92641bb6
Iswarya-vedhantham <134618427+Iswarya-vedhantham@users.noreply.github.com>
2023-12-12 21:53:37 +0530

    Add files via upload
    12-12-2023(1st day)

---
+++ pythonclass.py
@@ -0,0 +1,10 @@
+weight_kg = 68  # Weight in kilograms
+height_meter = 1.75  # Height in meters
+
+# Square the height
+height_squared = height_meter * height_meter
+
+# Calculate BMI using arithmetic operations
+bmi = weight_kg / height_squared
+
+print("Your BMI is:", round(bmi))
